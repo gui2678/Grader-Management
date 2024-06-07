466c0d6b803d145f0bcb1d6599e86fdf0c377234
thatjarrett <76824571+thatjarrett@users.noreply.github.com>
2024-06-07 16:07:52 -0400

    tried to put db again

--- grader_management/app/services/fetch_class_info.rb
+++ grader_management/app/services/fetch_class_info.rb
@@ -22,32 +22,25 @@ class FetchClassInfo
   def call
     response = self.class.get('/v2/classes/search', @options)
     if response.success?
-      process_response(response.parsed_response)
+      courses_data = response.parsed_response.dig('data', 'courses')
+      if courses_data
+        courses_data.each do |course_entry|
+          course_data = course_entry['course']
+          next unless course_data
+
+          course = Course.find_or_initialize_by(course_number: course_data['catalogNumber'])
+          course.course_name = course_data['title']
+          course.course_description = course_data['description']
+          course.credits = course_data['maxUnits']
+          course.save!
+        end
+        puts "Class information imported successfully."
+      else
+        puts "No courses found in the response."
+      end
     else
-      raise "Error fetching class info: #{response.code} - #{response.message}"
+      puts "Error fetching class info: #{response.code} - #{response.message}"
     end
-  end
-
-  private
-
-  def process_response(response_data)
-    courses_data = response_data.dig('data', 'courses')
-    
-    unless courses_data
-      raise "Unexpected response structure: 'courses' key not found"
-    end
-
-    courses_data.each do |course_entry|
-      course_data = course_entry['course']
-      next unless course_data
-
-      course = Course.find_or_initialize_by(course_number: course_data['catalogNumber'])
-      course.course_name = course_data['title']
-      course.course_description = course_data['description']
-      course.credits = course_data['maxUnits']
-      course.save!
-    end
-    puts "Class information imported successfully."
   end
 end
 
--- grader_management/db/migrate/20240607161419_create_courses.rb
+++ grader_management/db/migrate/20240607161419_create_courses.rb
@@ -1,12 +1,14 @@
 class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses do |t|
-      t.string :course_number
-      t.string :course_name
-      t.text :course_description
-      t.integer :credits
+      t.string :course_number, null: false
+      t.string :course_name, null: false
+      t.text :course_description, null: false
+      t.integer :credits, null: false
 
       t.timestamps
     end
+
+    add_index :courses, :course_number, unique: true
   end
 end
