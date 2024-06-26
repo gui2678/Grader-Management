887bb76c97f60c754d42b13a0ef536c75953763b
umarovel <123472975+umarovel@users.noreply.github.com>
2024-06-26 15:14:12 -0400

    Update user.rb

--- grader_management/app/models/user.rb
+++ grader_management/app/models/user.rb
@@ -20,15 +20,6 @@ class User < ApplicationRecord
   validates :role, presence: true
   validate :email_domain
 
-  # Method to check user permissions
-  def has_permission?(permission)
-    role_permissions = {
-      student: ['browse_catalog'],
-      instructor: ['browse_catalog'],
-      admin: ['browse_catalog', 'edit_catalog', 'reload_catalog', 'approve_registers']
-    }
-    role_permissions[role.to_sym].include?(permission)
-  end
   
   private
 
