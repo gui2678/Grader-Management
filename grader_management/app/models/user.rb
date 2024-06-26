892090c23b17d6085d8f68200afb7d727cdff7d4
thatjarrett <76824571+thatjarrett@users.noreply.github.com>
2024-06-26 11:51:40 -0500

    added error when using email other than osu

--- grader_management/app/models/user.rb
+++ grader_management/app/models/user.rb
@@ -8,14 +8,22 @@ class User < ApplicationRecord
   has_many :enrollments, foreign_key: 'student_id'
   has_many :approvals
 
-  #Define user roles
-  enum role: {student: 'student', instructor:'instructor', admin:'admin'}
+  # Define user roles
+  enum role: {student: 'student', instructor: 'instructor', admin: 'admin'}
 
   # Validations
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
   validates :password_confirmation, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :role, presence: true
+  validate :email_domain
+
+  private
+
+  def email_domain
+    domain = email.split('@').last
+    errors.add(:email, 'must be an @osu.edu email') unless domain == 'osu.edu'
+  end
 end
