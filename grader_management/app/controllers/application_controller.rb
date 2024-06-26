45f8e4894e4a911aa280e7c2befcf74c5dc22e2c
umarovel <123472975+umarovel@users.noreply.github.com>
2024-06-26 14:44:28 -0400

    Update application_controller.rb

--- grader_management/app/controllers/application_controller.rb
+++ grader_management/app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
 
-  def check_permission(permission)
-    unless current_user.has_permission?(permission)
-      flash[:alert] = "You do not have permission to perform this action."
-      redirect_to root_path
-    end
-  end
 end
