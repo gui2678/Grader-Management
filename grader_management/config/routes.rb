fabf2235f70f18ca52a27a0ddb73ede414047368
thatjarrett <76824571+thatjarrett@users.noreply.github.com>
2024-07-26 12:37:40 -0400

    fixed routes

--- grader_management/config/routes.rb
+++ grader_management/config/routes.rb
@@ -1,4 +1,34 @@
-pul  resources :courses do
+Rails.application.routes.draw do
+<<<<<<< HEAD
+  # Devise settings
+=======
+  # devise setting
+>>>>>>> e67b9746ebbd4c02dc2911aee4a1605213aedc37
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+
+  # User sign-out
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  # Admin routes
+  authenticated :user, ->(u) { u.admin? && u.approved? } do
+    namespace :admin do
+      get 'index'
+      get 'approve_requests'
+      get 'database-test', to: 'admin#test'
+      post 'fetch_class_info', to: 'admin#fetch_class_info'
+    end
+  end
+
+  # Home routes
+  get 'home/index'
+
+  # Course routes
+  resources :courses do
     collection do
       get 'reload_courses'
       post 'do_reload_courses'
