aa8645da235baef873b71064ac1f4ae2740a16e9
thatjarrett <76824571+thatjarrett@users.noreply.github.com>
2024-07-29 15:11:35 -0400

    merged main

--- grader_management/db/migrate/20240728165646_add_meeting_number_to_meetings.rb
+++ grader_management/db/migrate/20240728165646_add_meeting_number_to_meetings.rb
@@ -1,5 +1,7 @@
 class AddMeetingNumberToMeetings < ActiveRecord::Migration[7.0]
   def change
-    add_column :meetings, :meeting_number, :string
+    unless column_exists?(:meetings, :meeting_number)
+      add_column :meetings, :meeting_number, :string
+    end
   end
 end
