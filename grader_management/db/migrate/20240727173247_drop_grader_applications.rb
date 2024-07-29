b609b866903fed2c9847685c97ac164052a21184
nkaldhaheri <nk@aldhaheri.cc>
2024-07-29 04:52:24 +0200

    remove drop grader application migration file

--- grader_management/db/migrate/20240727173247_drop_grader_applications.rb
+++
@@ -1,9 +0,0 @@
-class DropGraderApplications < ActiveRecord::Migration[7.0]
-  def up
-    drop_table :grader_applications
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
