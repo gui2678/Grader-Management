09675c132647a2882bfb654627e4b0a5065d9ee1
thatjarrett <76824571+thatjarrett@users.noreply.github.com>
2024-07-30 14:26:17 -0400

    fixed navbar

--- grader_management/app/controllers/admin_controller.rb
+++ grader_management/app/controllers/admin_controller.rb
@@ -18,14 +18,14 @@ class AdminController < ApplicationController
         Rails.logger.debug "User approval status updated successfully for user: #{user.id}"
         redirect_to admin_index_path, notice: 'User approval status updated successfully.'
       else
-        Rails.logger.debug "Failed to update user approval status for user: #{user.id}"
+        Rails.logger.error "Failed to update user approval status for user: #{user.id}. Errors: #{user.errors.full_messages.join(', ')}"
         redirect_to admin_index_path, alert: 'Failed to update user approval status.'
       end
     else
-      redirect_to admin_index_path
+      Rails.logger.error "User not found with ID: #{params[:user_id]}"
+      redirect_to admin_index_path, alert: 'User not found.'
     end
   end
-  
 
   def test
     @courses = Course.all
@@ -41,7 +41,7 @@ class AdminController < ApplicationController
 
   def check_admin
     if current_user.admin?
-      if !current_user.approved?
+      unless current_user.approved?
         unless session[:unapproved_admin_alert_shown]
           flash[:alert] = 'Unapproved admin - your account will be restricted to student features until approval'
           session[:unapproved_admin_alert_shown] = true
